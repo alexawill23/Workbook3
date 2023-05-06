0a50929759d2d9be5a6d2bc5f5a4178df6ab8021
Alexa Williams <alexawill23@gmail.com>
2023-05-06 03:51:16 -0400

    redid simple_function exercise

---
+++ Functions/simple_functions.js
@@ -0,0 +1,20 @@
+"use strict"; 
+
+function favoriteThings () {
+    console.log("1. My name is Alexa"); 
+    console.log("My favorite movie is Mean Girls"); 
+    console.log("My favorite muscian is Beyonce"); 
+
+}
+favoriteThings()
+
+function whyImHere() {
+    console.log("2. I joined the Year Up Program to start a new career path and to join an on-going field that I won't get easily bored in.");
+} 
+whyImHere()
+
+function favoritePlace(){
+    console.log("3. One of my favorite places to visit is Jamaica becuase of the culture, food, and beaches.");
+
+}
+favoritePlace()
