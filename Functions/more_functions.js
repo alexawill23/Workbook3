a5fd83030fc93767ac031666c29d2e984cb006bd
Alexa Williams <alexawill23@gmail.com>
2023-05-07 00:02:00 -0400

    redid address exercise

---
+++ Functions/more_functions.js
@@ -0,0 +1,12 @@
+"use strict"; 
+
+"use strict";
+
+function displayMailingLabel(name, address, city, state, zip) {
+  console.log(name);
+  console.log(address);
+  console.log(city + ", " + state + " " + zip);
+}
+
+displayMailingLabel("Alexa Williams", "5673 Cherry Street", "Walker", "Ohio", 98569);
+
